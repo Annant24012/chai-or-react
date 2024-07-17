1c92d5ba324ace1cb21bccb50d8a1ad854f82cf1
Annant24012 <annantgupta12@gmail.com>
2024-07-17 12:30:55 +0530

    chai or react

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -3,10 +3,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [color,setcolor]=useState("Red");
+  const [color,setcolor]=useState("pink");
   let mybody = document.getElementById('mybody')
 
   mybody.style.backgroundColor=color;
+  function  colorChanger(event){
+    let c = event.target.value ;
+    setcolor(c);
+  }
   return (
     <div  className="container">
      <h1>BACKGROUND CHANGER</h1>
@@ -16,6 +20,9 @@ function App() {
      <button onClick={()=>{setcolor("pink")}}  class="btn" style={{backgroundColor:"pink"}}>PINK</button>
      <button onClick={()=>{setcolor("green")}} class="btn" style={{backgroundColor:"green"}}>GREEN</button>
      <button onClick={()=>{setcolor("yellow")}} class="btn" style={{backgroundColor: "yellow"}}>YELLOW</button>
+     <input type="color" id="pickcolor"  onChange={colorChanger} class="btn" className='p-5 shadow-xl rounded-2xl'/>
+
+     <div className='text-red-800 bg-amber-300 size-9 mx-11' >hello</div>
     </div>
   )
 }
